ad419f91d4b38bfec4f8952a23de2e69c90f19b6
ryanpersad <Rpersad0305@gmail.com>
2021-06-06 19:54:53 -0400

    Initial commit

---
+++ src/main/java/MathCalc/App.java
@@ -0,0 +1,33 @@
+/**
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Ryan Persad
+ */
+package MathCalc;
+import java.util.Scanner;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        Scanner myObj = new Scanner(System.in);
+        System.out.println( "What is the first number?" );
+        //Input
+        String num1  =  myObj.nextLine();
+        System.out.println( "What is the second number?" );
+        //Input
+        String num2  =  myObj.nextLine();
+        //Convert to int
+        int newNum1 = Integer.parseInt(num1);
+        int newNum2 = Integer.parseInt(num2);
+        //Does arithmetic
+        int add = newNum1 + newNum2;
+        int sub = newNum1 - newNum2;
+        int multiply = newNum1 * newNum2;
+        int divide = newNum1 / newNum2;
+        String print = String.format("%s + %s = %d\n%s - %s = %d\n%s * %s = %d\n%s / %s = %d",
+                num1,num2,add,num1,num2,sub,num1,num2,multiply,num1,num2,divide);
+        //Output
+        System.out.println(print);
+
+    }
+}
